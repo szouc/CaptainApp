b8441398d009dac3f0000cfa91cffc8ab1e4a9a1
szouc <szouc@126.com>
2018-10-12 00:01:48 +0800

    show picture in ReviewList

--- src/modules/transport/components/TransportCardList/ReviewList/index.js
+++ src/modules/transport/components/TransportCardList/ReviewList/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import InputList from '../InputList'
 import OrderList from '../OrderList'
+import { List } from 'antd-mobile'
+import { Image } from 'react-native'
+import { WEB_ADDR } from '../../../../../settings/configs'
 
 class TransportReviewList extends React.PureComponent {
   constructor(props) {
@@ -12,6 +15,16 @@ class TransportReviewList extends React.PureComponent {
       <React.Fragment>
         <OrderList transport={this.props.transport} />
         <InputList transport={this.props.transport} />
+        <List renderHeader={() => '运输照片'}>
+          <Image
+            style={{ flex: 1, width: '100%', height: 400 }}
+            source={{
+              uri: this.props.transport.shippingPic
+                ? `${WEB_ADDR}${this.props.transport.shippingPic}`
+                : ''
+            }}
+          />
+        </List>
       </React.Fragment>
     )
   }
